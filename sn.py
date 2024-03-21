c92e5ed6e747e52c0320a62584f8788f81544c66
HajnalVince <114928119+HajnalVince@users.noreply.github.com>
2024-03-21 17:38:21 +0100

    Add files via upload

---
+++ sn.py
@@ -0,0 +1,6 @@
+f = open("snooker.txt")
+
+elsosor = f.readline()
+lista = f.readlines()
+versenyzok_szama = len(lista)
+print(f"5.2. feladat: A világranglistán {versenyzok_szama} versenyző szerepel")
